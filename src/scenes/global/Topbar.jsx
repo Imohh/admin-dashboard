8a2425db7c18ead6bb68d63120baeff7123c71a5
Imoh Precious <jaypee88830@gmail.com>
2023-08-03 04:21:53 +0100

    topbar

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import { Box, IconButton, useTheme } from "@mui/material"
+import { ColorModeContext, tokens } from "../../theme"
+import InputBase from "@mui/material/InputBase"
+import LightModeOutlined  from "@mui/icons-material/LightModeOutlined"
+import DarkModeOutlined  from "@mui/icons-material/DarkModeOutlined"
+import NotificationsOutlined  from "@mui/icons-material/NotificationsOutlined"
+import SettingsOutlined from "@mui/icons-material/SettingsOutlined"
+import PersonOutlined from "@mui/icons-material/PersonOutlined"
+import SearchIcon from "@mui/icons-material/Search"
+
+const Topbar = () => {
+	const theme = useTheme()
+	const colors = tokens(theme.pallete.mode)
+	const colorMode = useContext(ColorModeContext)
+
+	return (
+		<Box display="flex" justifyContent="space-between" p={2}>
+			{/*SEARCH BAR*/}
+			<Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+				<InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+				<IconButton>
+					<SearchIcon />
+				</IconButton>
+			</Box>
+
+			{/*ICONS*/}
+			<Box display="flex">
+				<IconButton omClick={colorMode.toggleColorMode}>
+					{theme.pallate.mode === 'dark' ? (
+						<DarkModeOutlined />
+					) : (
+						<LightModeOutlined />
+					)}
+				</IconButton>
+				<IconButton>
+					<NotificationsOutlined />
+				</IconButton>
+				<IconButton>
+					<SettingsOutlined />
+				</IconButton>
+				<IconButton>
+					<PersonOutlined />
+				</IconButton>
+			</Box>
+
+		</Box>
+	)
+}
+
+export default Topbar
